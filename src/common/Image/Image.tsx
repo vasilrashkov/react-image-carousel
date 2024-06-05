54978bcd9a97e776dda19697d3d5d54d9404c19d
Vasil Rashkov <vas.rashkov@gmail.com>
2024-06-05 12:19:34 +0300

    add check if image did not load

--- src/common/Image/Image.tsx
+++ src/common/Image/Image.tsx
@@ -1,19 +1,29 @@
-import { ForwardedRef, forwardRef } from "react";
+import { ForwardedRef, forwardRef, useState } from "react";
 import styled from "styled-components";
 
 type ImageComponentProps = {
     src: string;
     alt?: string;
     className?: string;
-    width?: string;
-    ref?: any;
-    height?: string;
     maxHeight?: number;
     maxWidth?: number;
     style?: React.CSSProperties;
 
     onClick?: () => void;
 };
+
+const ErrorContainer = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: #f1f1f1;
+    color: #000;
+    font-size: 1.5rem;
+    font-weight: 500;
+    text-align: center;
+    border-radius: 10px;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+`;
 
 const StyledImage = styled.img`
     ${props => props.style?.maxHeight && `max-height: ${props.style?.maxHeight}px;`}
@@ -28,15 +38,26 @@ const Image: React.FC<ImageComponentProps> = forwardRef(function Image({
     style,
     alt = "image-component",
     className,
-    width,
-    height,
     maxHeight,
     maxWidth,
     onClick,
 }: ImageComponentProps, ref: ForwardedRef<HTMLImageElement>) {
+    const [errorLoadingImage, setErrorLoadingImage] = useState<boolean>(false);
+
+    const onError = () => {
+        setErrorLoadingImage(true);
+    };
+
+    if (errorLoadingImage) return (
+        <ErrorContainer style={{ ...style }}>
+            Error loading image
+        </ErrorContainer>
+    );
+
     return (
         <StyledImage
             ref={ref}
+            onError={onError}
             src={src}
             style={{...style, maxHeight, maxWidth}}
             alt={alt}
